e4914b751f7b21a96b1473ebc872dd7a63d2cb8e
TheCarmelGuy <komelm13@gmail.com>
2025-03-22 13:14:25 -0400

    got rid of stuf

--- orchestrators/classifier_module.py
+++ orchestrators/classifier_module.py
@@ -16,4 +16,4 @@ class LitClassifier(L.LightningModule):
         return loss
     
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3, momentum=0.9)
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
